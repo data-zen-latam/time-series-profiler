727becc6abae3dc501dcae73dd11ce421eb9489c
Carlo <carlo.aariasg@outlook.com>
2024-09-24 14:25:50 -0600

    [ADD] Reports DIR

--- research/template_processing.py
+++ research/template_processing.py
@@ -2,12 +2,15 @@
 
 import pandas as pd
 import numpy as np
+import os
+from pathlib import Path
 
 from ts_profiler.config import PROCESSED_DATA_DIR, REPORTS_DIR
 from jinja2 import Environment, FileSystemLoader
 import plotly.express as px
 #%%
-
+PROCESSED_DATA_DIR = Path(os.getcwd()).parents[0] / 'data/processed/'
+REPORTS_DIR = Path(os.getcwd()).parents[0] / 'reports/'
 data = pd.read_csv(PROCESSED_DATA_DIR / 'features.csv')
 df_stats = pd.read_csv(PROCESSED_DATA_DIR / 'descriptive_stats.csv')
 df_values = pd.read_csv(PROCESSED_DATA_DIR / 'values_summary.csv')
