66393173891f6f06bdb37a8113de1659fd94764e
Carlo <carlo.aariasg@outlook.com>
2024-11-29 13:35:07 -0600

    [ADD] Scoring Process

--- research/feature_calculation.py
+++ research/feature_calculation.py
@@ -10,7 +10,7 @@ from pathlib import Path
 
 #%%
 # input_path = PROCESSED_DATA_DIR / 'diff_temp_series.parquet.gzip'
-PROCESSED_DATA_DIR = Path(os.getcwd()) / 'data/processed/'
+PROCESSED_DATA_DIR = Path(os.getcwd()).parents[0] / 'data/processed/'
 REPORTS_DIR = Path(os.getcwd()) / 'reports/'
 input_path = PROCESSED_DATA_DIR / 'diff_temp_series.parquet.gzip'
 output_path = REPORTS_DIR / 'timeseries_diagnostics.html'
@@ -38,8 +38,71 @@ for c in data.drop(['date'], axis=1):
         pd.DataFrame([stats], index= [0])
     ])
 
+#%%
+aux = data.melt(id_vars='date', var_name='series')
+
+result['upper_threshold'] = result['75%'] + 1.5 * result['iqr']
+result['lower_threshold'] = result['25%'] - 1.5 * result['iqr']
+
+aux = aux.merge(result[['series', 'upper_threshold', 'lower_threshold']], how='left', on='series')
+
+aux['p_outliers'] = (aux['value'] > aux['upper_threshold']) | (aux['value'] < aux['lower_threshold'])
+aux = aux.groupby(['series'])['p_outliers'].mean()
+
+result = pd.concat([result.set_index('series'), aux], axis=1)
 
 
+#%%
+score_cols = [   # 'cv',
+    'p_distinct',
+    'p_missing',
+    'p_zeros',
+    'p_outliers',
+    'entropy',
+]
+
+aux = result[score_cols]
+
+aux['score_distinct'] = aux['p_distinct']
+aux['score_missing'] = 1 - aux['p_missing']
+aux['score_zeros'] = 1 - aux['p_zeros']
+aux['score_outliers'] = 1 - aux['p_outliers']
+aux['score_entropy'] = 1 - (aux['entropy'] / aux.loc['w_noise', 'entropy'])
+# TODO : Corregir distribucion de white noise con parametros para cada series / monte carlo de series con entropia
+#%%
+aux['completeness_score'] = aux.loc[:, aux.columns.str.contains('score')].mean(axis=1)
+
+scores_ = aux.copy(deep=True)
+#%%
+### Forecastability
+score_forecast = [ 'entropy',
+ 'strength_trend',
+ 'strength_seasonality',
+ 'kurtosis',
+ 'adfuller',
+ 'shapiro',
+ 'fourier_entropy'
+ ]
+
+aux = result[score_forecast]
+aux['flag_adf'] = aux['adfuller'] > 0.05 # Not Reject Null Hypothesis ie There is Unit Root
+aux['flag_shapiro'] = aux['shapiro'] < 0.05 # Reject Null Hypothesis ie Non Normal Distribution
+aux['flag_kurtosis'] = aux['kurtosis'] > 3
+
+aux['score_flags'] = 1 - aux.loc[:, aux.columns.str.contains('flag')].sum(axis=1) / np.sum(aux.columns.str.contains('flag'))
+aux['score_trend'] = aux['strength_trend']
+aux['score_seasonality'] = aux['strength_seasonality']
+aux['score_entropy'] = 1 - (aux['entropy'] / aux.loc['w_noise', 'entropy'])
+
+
+aux['predictability_score'] = aux.loc[:, aux.columns.str.contains('score')].mean(axis=1)
+
+scores_ = pd.concat([scores_, aux], axis=1)
+
+#%%
+import seaborn as sns
+
+sns.scatterplot(scores_, x='completeness_score', y='predictability_score')
 
 #%%
 result[features].to_csv(PROCESSED_DATA_DIR / 'features.csv', index=False)
--- research/m3_processing.py
+++ research/m3_processing.py
@@ -1,5 +1,6 @@
 #%%
 import pandas as pd
+from scipy.stats import entropy
 from ts_profiler.config import RAW_DATA_DIR
 from ts_profiler.config import features, descriptive_stats, value_summary
 from ts_profiler.features import funcs
@@ -42,7 +43,10 @@ series = list(itertools.chain(*aux))
 series = {name : serie for name, serie in series}
 metadata = pd.concat([ metadata for serie, metadata in extraction])
 
-
+mu = 0
+std = 1
+series['w_noise'] = np.random.normal(mu, std, size=series.get('N1402').shape[0])
+series['sine'] = np.sin(np.arange(0,series.get('N1402').shape[0]))
 #%%
 result = pd.DataFrame()
 for k, serie in series.items():
@@ -57,8 +61,103 @@ for k, serie in series.items():
         result,
         pd.DataFrame([stats], index= [0])
     ])
+#%%
+
+from operator import itemgetter
+
+x = data['month']['Series'].to_numpy()
+x = np.concat([x, ['w_noise', 'sine']])
+
+a = itemgetter(*x)(series)
+
+a = np.array(a)
+df = pd.DataFrame(a).T
+df.columns = x
+df['index'] = np.arange(0, df.shape[0])
+
+#%%
+result = result[np.isin(result['series'], x)]
+
+#%%
 
 
+aux = df.melt(id_vars='index', var_name='series')
+
+result['upper_threshold'] = result['75%'] + 1.5 * result['iqr']
+result['lower_threshold'] = result['25%'] - 1.5 * result['iqr']
+
+aux = aux.merge(result[['series', 'upper_threshold', 'lower_threshold']], how='left', on='series')
+
+aux['p_outliers'] = (aux['value'] > aux['upper_threshold']) | (aux['value'] < aux['lower_threshold'])
+aux = aux.groupby(['series'])['p_outliers'].mean()
+
+result = pd.concat([result.set_index('series'), aux], axis=1)
+
+
+#%%
+score_cols = [   # 'cv',
+    'p_distinct',
+    'p_missing',
+    'p_zeros',
+    'p_outliers',
+    'entropy',
+]
+
+aux = result[score_cols]
+
+aux['score_distinct'] = aux['p_distinct']
+aux['score_missing'] = 1 - aux['p_missing']
+aux['score_zeros'] = 1 - aux['p_zeros']
+aux['score_outliers'] = 1 - aux['p_outliers']
+aux['score_entropy'] = 1 - (aux['entropy'] / entropy(np.ones(df.shape[0]) / df.shape[0]))
+
+#%%
+aux['completeness_score'] = aux.loc[:, aux.columns.str.contains('score')].mean(axis=1)
+
+scores_ = aux.copy(deep=True)
+#%%
+### Forecastability
+score_forecast = [ 'entropy',
+ 'strength_trend',
+ 'strength_seasonality',
+ 'kurtosis',
+ 'adfuller',
+ 'shapiro',
+ 'fourier_entropy'
+ ]
+
+aux = result[score_forecast]
+aux['flag_adf'] = aux['adfuller'] > 0.05 # Not Reject Null Hypothesis ie There is Unit Root
+aux['flag_shapiro'] = aux['shapiro'] < 0.05 # Reject Null Hypothesis ie Non Normal Distribution
+aux['flag_kurtosis'] = aux['kurtosis'] > 3
+
+aux['score_flags'] = 1 - aux.loc[:, aux.columns.str.contains('flag')].sum(axis=1) / np.sum(aux.columns.str.contains('flag'))
+aux['score_trend'] = aux['strength_trend']
+aux['score_seasonality'] = aux['strength_seasonality']
+# aux['score_entropy'] = 1 - (aux['entropy'] / entropy(np.ones(df.shape[0]) / df.shape[0]))
+# switch for fourier entropy
+
+aux['predictability_score'] = aux.loc[:, aux.columns.str.contains('score')].mean(axis=1)
+
+scores_ = pd.concat([scores_, aux], axis=1)
+
+#%%
+import seaborn as sns
+
+sns.scatterplot(scores_, x='completeness_score', y='predictability_score')
+
+#%%
+mask = ((scores_['predictability_score'] < 0.2) & (scores_['completeness_score'] <0.82))
+
+scores_[mask].T
+scores_.iloc[-2:,:].T
+#%%
+x = series.get('N2755')
+sns.lineplot(x=np.arange(0, df.shape[0]), y=x)
+
+# x = series.get('N1850')
+x = series.get('N2573')
+sns.lineplot(x=np.arange(0, df.shape[0]), y=x)
 # %%
 data_features = result[features]
 # %%
